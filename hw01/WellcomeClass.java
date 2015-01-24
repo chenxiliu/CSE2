eaa54f4076233ea9279b90d17f871832450d88a6
chenxiliu <cxl216@lehigh.edu>
2015-01-24 22:50:30 +0000

    update WellcomeClass

---
+++ hw01/WellcomeClass.java
@@ -0,0 +1,32 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//Chenxi Liu
+//HW 01
+//WelcomeClass Java Program
+//
+//  first compile program
+//        javac WellcomeClass.java
+//  run the program
+//        java WellcomeClass//
+
+//define a class
+public class WellcomeClass{
+
+//  add main method    
+    public static void main(String[] args) {
+
+//  print the welcome statement with lehigh network ID    
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^ ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-C--X--L--2--1--6->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I major in mechanical engineering, minor in aerospace,");
+        System.out.println("I also plan to get a minor in CSE.");
+        
+        //autobiographic statement
+    
+    }
+}
